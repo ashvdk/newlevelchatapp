e190d7c72987b4e4879a11c1687e8329a1bc69fc
ashwin rao <ashwinvdrk@gmail.com>
2021-09-14 01:19:01 +0530

    designing chat screen

--- src/components/Container.js
+++ src/components/Container.js
@@ -18,8 +18,8 @@ function Container() {
                         <img src="https://i.etsystatic.com/9193132/r/il/93fcac/1374669102/il_794xN.1374669102_kk7g.jpg" style={{width: "100%"}}/>
                     </div>
                     <div style={{display: "flex", flex: 1, flexDirection: "column", justifyContent: "center", padding: "01px 10px"}}>
-                        <div>Ashwin Rao K</div>
-                        <div>She will reply to you tomorrow</div>
+                        <div style={{fontWeight: "bold"}}>Ashwin Rao K</div>
+                        <div  style={{color: "grey", fontSize: "14px"}}>She will reply to you tomorrow</div>
                     </div>
                     <div style={{display: "flex", alignItems: "center"}}>
                         Yesterday
